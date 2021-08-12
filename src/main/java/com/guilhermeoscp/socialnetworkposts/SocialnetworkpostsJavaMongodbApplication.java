576f1c0d7f5cb057253a66c1261e59cecf811277
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-08-12 10:10:41 -0300

    Inicio do Projeto

---
+++ src/main/java/com/guilhermeoscp/socialnetworkposts/SocialnetworkpostsJavaMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeoscp.socialnetworkposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkpostsJavaMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkpostsJavaMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeoscp/socialnetworkposts/SocialnetworkpostsJavaMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeoscp.socialnetworkposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkpostsJavaMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
