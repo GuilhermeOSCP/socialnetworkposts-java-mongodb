652b5c52333c6d251a93a44e2a921904fa9ddf55
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-08-13 09:48:56 -0300

    Implementacao de usuario com POST

--- src/main/java/com/guilhermeoscp/socialnetworkposts/services/UserService.java
+++ src/main/java/com/guilhermeoscp/socialnetworkposts/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermeoscp.socialnetworkposts.domain.User;
+import com.guilhermeoscp.socialnetworkposts.dto.UserDTO;
 import com.guilhermeoscp.socialnetworkposts.repository.UserRepository;
 import com.guilhermeoscp.socialnetworkposts.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getEmail(), objDto.getEmail());
+	}
 }
