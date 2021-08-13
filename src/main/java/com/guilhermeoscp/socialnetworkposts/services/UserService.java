263e01a272aa86c7e6500e694fe7e748cce76028
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-08-13 09:13:21 -0300

    Implementacao de Obtencao de usuario por id

--- src/main/java/com/guilhermeoscp/socialnetworkposts/services/UserService.java
+++ src/main/java/com/guilhermeoscp/socialnetworkposts/services/UserService.java
@@ -1,12 +1,14 @@
 package com.guilhermeoscp.socialnetworkposts.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermeoscp.socialnetworkposts.domain.User;
 import com.guilhermeoscp.socialnetworkposts.repository.UserRepository;
+import com.guilhermeoscp.socialnetworkposts.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
---
+++ src/main/java/com/guilhermeoscp/socialnetworkposts/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.guilhermeoscp.socialnetworkposts.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
